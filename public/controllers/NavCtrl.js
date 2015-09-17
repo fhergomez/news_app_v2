4636bb65a23276bf82c2d726082f61c867675ecf
Fernando Gomez <fernandogomez37@yahoo.com>
2015-09-17 15:16:58 -0700

    hamburger button working

--- public/controllers/NavCtrl.js
+++ public/controllers/NavCtrl.js
@@ -2,6 +2,7 @@ NewsApp.controller('NavCtrl', ['$scope','$routeParams','auth', function ($scope,
   console.log('Nav ctrl');
   // console.log('auth',auth.isLoggedIn(),auth.currentUser());
   // $scope.auth = auth;
+  $scope.navCollapsed = true;
   $scope.isLoggedIn = auth.isLoggedIn;
   $scope.currentUser = auth.currentUser;
   $scope.logOut = auth.logOut;

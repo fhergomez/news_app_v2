bb0c23c92807c7528b412a21c4d82a689874df78
Fernando Gomez <fernandogomez37@yahoo.com>
2015-09-17 13:04:41 -0700

    functional

--- public/controllers/MainCtrl.js
+++ public/controllers/MainCtrl.js
@@ -5,15 +5,17 @@ NewsApp.controller('MainCtrl', ['$scope', '$routeParams','$http','$modal','posts
   $scope.isLoggedIn = auth.isLoggedIn;
 
   $scope.data = {};
-   $http.get('/api/news').success(function(data){
+  $http.get('/api/news').success(function(data){
     console.log(data);
     $scope.results = data.results;
-   });
+  });
   // posts.get($routeParams.id).success(function(post){
   //   $scope.post = post;
   // });
 
   $scope.posts = posts.posts;
+  $scope.title = '';
+  $scope.link = '';
 
   $scope.addPost = function() {
     if (!$scope.title || $scope.title === '') {
@@ -24,7 +26,7 @@ NewsApp.controller('MainCtrl', ['$scope', '$routeParams','$http','$modal','posts
       link: $scope.link
     });
     $scope.title = '';
-    $scope.link = $scope.link + 'target="_blank"';
+    $scope.link = '';
   }
 
   $scope.incrementUpvotes = function(post) {
